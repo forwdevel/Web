d372754b73513f12f3205fc563133572c3ce6bfa
forwdevel <forwdevel@gmail.com>
2022-05-30 19:24:05 +0900

    Clone Project Updated
    "samsung.com/us" web clone project started.

---
+++ Project/Clone Web/Samsung/js/header.js
@@ -0,0 +1,8 @@
+let sideMenu = document.querySelector('.side-menu-inner');
+let firstDrop = document.querySelector('.first-drop-ul');
+
+
+
+sideMenu.addEventListener( "mouseover", function (event) {
+    target.firstDrop.style.display = "block";
+});
