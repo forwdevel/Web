6aff3a2bf78c8a60cdf8084a3fb6f651414c3d44
forwdevel <forwdevel@gmail.com>
2022-05-31 23:00:09 +0900

    Clone project updated
    samsung updated

--- Project/Clone Web/Samsung/js/header.js
+++ Project/Clone Web/Samsung/js/header.js
@@ -1,8 +1,38 @@
-let sideMenu = document.querySelector('.side-menu-inner');
-let firstDrop = document.querySelector('.first-drop-ul');
+const sideAnchor1 = document.getElementById("side1-anchor");
+const sideAnchor2 = document.getElementById("side2-anchor");
+const sideAnchor3 = document.getElementById("side3-anchor");
+
+const side1 = document.getElementById("side1-ul");
+const side2 = document.getElementById("side2-ul");
+const side3 = document.getElementById("side3-ul");
+
+const lastside = document.getElementById('last-side-li');
 
 
 
-sideMenu.addEventListener( "mouseover", function (event) {
-    target.firstDrop.style.display = "block";
-});
+// Side-menu 1st
+sideAnchor1.addEventListener( "mouseover", (event) => {
+    side1.style.display = "block";
+}, false);
+
+sideAnchor1.addEventListener("mouseout", (event) => {
+    side1.style.display = "none";
+}, false);
+
+// Side-menu 2nd
+sideAnchor2.addEventListener( "mouseover", (event) => {
+    side2.style.display = "block";
+}, false);
+
+sideAnchor2.addEventListener("mouseout", (event) => {
+    side2.style.display = "none";
+}, false);
+
+// Side-menu 3rd
+sideAnchor3.addEventListener( "mouseover", (event) => {
+    side3.style.display = "block";
+}, false);
+
+sideAnchor3.addEventListener("mouseout", (event) => {
+    side3.style.display = "none";
+}, false);
