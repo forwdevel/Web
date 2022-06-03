e8b0f68f859f9f011a9d3c15ebc7847e3b265859
forwdevel <forwdevel@gmail.com>
2022-06-03 13:27:53 +0900

    02/Jun/22
    swiper and progress bar study and clone web are updated

--- Project/Clone Web/Samsung/js/swiper.js
+++ Project/Clone Web/Samsung/js/swiper.js
@@ -4,38 +4,6 @@
 
 // [ testing ]
 
-const progress = document.querySelector(".progress");
-
-const swiper = new Swiper(".mySwiper", {
-  loop: true,
-  loopedSlides: 6,
-  pagination: {
-    el: ".swiper-pagination",
-    type: "bullets",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-  on: {
-    init: function () {
-      progress.classList.remove("animate");
-      progress.classList.add("animate");
-    },
-    slideChangeTransitionStart: function () {
-      progress.classList.remove("animate");
-    },
-    slideChangeTransitionEnd: function () {
-      progress.classList.add("animate");
-    },
-  },
-});
-
 // //Implemeting a swiper that switches the screen
 // // when text is pressed
 
